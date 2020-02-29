19b61706778fbc26e301f0c35810b3c56f69a728
wotjd4305 <43901682+wotjd4305@users.noreply.github.com>
2020-02-29 18:59:05 +0900

    Add files via upload

--- 0226_0304/박재성/bac17472_다리만들기2_G3.java
+++ 0226_0304/박재성/bac17472_다리만들기2_G3.java
@@ -3,16 +3,13 @@ package main.java.Temp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class bac17472_다리만들기2_G3 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int R;
     static int C;
-    final static int M = 99;
+    final static int M = 9999999;
     static int answer = 0;
 
     static int map[][];
@@ -40,76 +37,38 @@ public class bac17472_다리만들기2_G3 {
 
         check_island();
         init_map_and_distance();
-        kruskal();
-       // mpprint();
-       // System.out.println();
-       // pprint();
+        answer = getMinimumDistance();
 
-        if(answer == 0)
-            answer = -1;
         System.out.println(answer);
 
     }
 
-    private static void kruskal() {
-        //구한 최소거리를 다시 정렬할 수 있게 가져와서 초기화
-        int idx = 0;
-        int length=0;
+    //Kruskal
+    private static int getMinimumDistance() {
+        ArrayList<Edge> G = new ArrayList<>();
+        int distance = 0;
+        //makeset
+        for (int i = 1; i < p.length; i++) p[i] = i;
 
-        //연결된 엣지 갯수 구하기, p.length는 섬의 갯수 +1
-        for(int i =1; i<p.length; i++){
-            for(int j=i+1; j<p.length; j++){
-                if(val[i][j] != M)
-                   length++;
+        for (int i = 1; i < p.length; i++) {
+            for (int j = i; j < p.length; j++) {
+                if (val[i][j] == M) continue;
+                G.add(new Edge(i, j, val[i][j]));
             }
         }
-
-        //
-        Edge G[] = new Edge[length];
-        for(int i =1; i<p.length; i++){
-            for(int j=i+1; j<p.length; j++){
-                if(val[i][j] != M) {
-                    G[idx++] = new Edge(i, j, val[i][j]);
-                }
-            }
-        }
-        Arrays.sort(G);
-      /* for(Edge e : G)
-          System.out.println(e);
-        System.out.println();*/
-
-        //부모를 자신으로  makeset
-        //섬 1,2,3,4 (p.lenght은 5)
-        for(int i =1; i<p.length; i++)
-            p[i] = i;
-
-        int cnt = 0;
-        int MST = 0;
-
-        //섬 숫자(N)보다 변수가 N-1 보다 적을 때.
-        if(length < ((p.length-1)-1)){
-            answer = -1;
-            return;
-        }
-
-        for(int i =0; i<length; i++){
-            Edge tmp = G[i];
-            int px = findSet(tmp.x);
-            int py = findSet(tmp.y);
-            if(px != py){
+        Collections.sort(G);
+        int cnt = 1;
+        for (int i = 0; i < G.size(); i++) {
+            int px = findSet(G.get(i).x);
+            int py = findSet(G.get(i).y);
+            if (px != py) {
+                distance += G.get(i).val;
                 union(px, py);
-               //System.out.println(tmp);
                 cnt++;
-                MST += tmp.val;
-                if(cnt == length)
-                    break;
+                if(cnt == p.length -1) break;
             }
-
         }
-
-       answer = MST;
-
-
+        return cnt == p.length - 1 ? distance : -1;
     }
 
     private static void union(int px, int py) {
@@ -117,11 +76,8 @@ public class bac17472_다리만들기2_G3 {
     }
 
     private static int findSet(int x) {
-        if(p[x] == x){
-            return x;
-        }
-        else
-            return p[x] = findSet(p[x]);
+        if(p[x] == x)return x;
+        return p[x] = findSet(p[x]);
     }
 
 
@@ -133,35 +89,33 @@ public class bac17472_다리만들기2_G3 {
 
         for(int i =0; i<R; i++){
             for(int j=0; j<C; j++){
+                if(map[i][j] == 0) continue;
                 int num = map[i][j];
-               // System.out.println(num);
 
                 for(int d=0; d<4; d++){
                     int count = 0;
                     int new_x = i + dy[d];
                     int new_y = j + dx[d];
                     //범위안이고
-                        while(true){
-                            //1)범위 벗어남? 종료.
-                            if(!(new_x >=1 && new_y >=1 && new_x<R && new_y <C))
-                                break;
+                    while(true){
+                        //1)범위 벗어남? 종료.
+                        if(!(new_x >=0 && new_y >=0 && new_x<R && new_y <C))
+                            break;
                             //2)바다니? 그럼 그 방향으로 계쏙 가봐
-                            if(map[new_x][new_y] == 0){
-                                count++;
-                                new_x += dy[d];
-                                new_y += dx[d];
-                            }//3) 같은 섬이면 종료.
-                            else if(map[new_x][new_y] == num)
-                                break;
-                            else { // 다른 섬과 마주친것
-                                if(count <2)//다리길이는 2이상입니다.
-                                    break;
-                                int min = val[num][map[new_x][new_y]];
-                                min = Math.min(min, count);
-                                val[num][map[new_x][new_y]] = min;
-                                break;
-                            }
+                        else if(map[new_x][new_y] == 0){
+                            count++;
+                            new_x += dy[d];
+                            new_y += dx[d];
                         }
+                        else { //3) 같은섬? 4) 다른 섬과 마주친것
+                            if(count <2 || map[new_x][new_y] == num)//다리길이는 2이상입니다.
+                                break;
+                            int min = val[num][map[new_x][new_y]];
+                            min = Math.min(min, count);
+                            val[num][map[new_x][new_y]] = min;
+                            break;
+                        }
+                    }
                 }// for d
             }// for j
         }// for i
@@ -185,11 +139,11 @@ public class bac17472_다리만들기2_G3 {
 
     //섬마다 번호를 줄것임.
     private static void BFS(int x, int y, int idx) {
-        Queue<Point> Q = new LinkedList<>();
-        Q.add(new Point(x,y));
+        Queue<Edge> Q = new LinkedList<>();
+        Q.add(new Edge(x,y));
 
         while(!Q.isEmpty()){
-            Point tmp = Q.poll();
+            Edge tmp = Q.poll();
             map[tmp.x][tmp.y] = idx;
             visited[tmp.x][tmp.y] = true;
 
@@ -198,41 +152,13 @@ public class bac17472_다리만들기2_G3 {
                 int new_y = tmp.y + dx[i];
                 if(new_x >=0 && new_y >=0 && new_x<R && new_y <C){
                     if(!visited[new_x][new_y] && map[new_x][new_y] == 1) {
-                        Q.add(new Point(new_x, new_y));
+                        Q.add(new Edge(new_x, new_y));
                     }
                 }
 
             }
         }
 
-    }
-
-    public static void mpprint(){
-        for(int i =0; i<val.length; i++){
-            for(int j=0; j<val.length; j++){
-                System.out.printf("%2d ", val[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-    public static void pprint(){
-
-        for(int i =0; i<R; i++){
-            for(int j=0; j<C; j++){
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static class Point{
-        int x;
-        int y;
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 
     public static class Edge implements Comparable<Edge>{
@@ -247,17 +173,14 @@ public class bac17472_다리만들기2_G3 {
             this.val = val;
         }
 
-        // 디버깅이 편함
-        @Override
-        public String toString() {
-            return "(" + x + "," + y + "=" + val;
+        public Edge(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
 
         @Override
         public int compareTo(Edge o) {
             return this.val - o.val;
         }
-
-
     }
 }
